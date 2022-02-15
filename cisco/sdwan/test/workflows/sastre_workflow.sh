3fd2719e856c391f0aa33ef833e3cc7e20b42a7f
sakamava <sakamava@cisco.com>
2022-02-15 15:38:42 +0530

    CXTM  automation script

--- cisco/sdwan/test/workflows/sastre_workflow.sh
+++ cisco/sdwan/test/workflows/sastre_workflow.sh
@@ -5,7 +5,6 @@ echo $value | grep -o "version: [0-9]*\.[0-9]*\.[0-9]*" | sed "s/version: //g" >
 version=`cat version.txt`
 ansible-galaxy collection build --force
 ansible-galaxy collection install cisco-sdwan-$version.tar.gz --force
-export VMANAGE_PASSWORD=$VMANAGE_PASSWORD
 export VMANAGE_IP=$VMANAGE_IP
 export VMANAGE_PORT=$VMANAGE_PORT
 export VMANAGE_USER=$VMANAGE_USER
@@ -13,7 +12,7 @@ export VMANAGE_PASSWORD=$VMANAGE_PASSWORD
 cd ../../
 mkdir temp
 cd temp
-git clone https://wwwin-github.cisco.com/sakamava/Sastre-Ansible.git
+git clone https://wwwin-github.cisco.com/AIDE/Sastre-Ansible.git
 cd Sastre-Ansible
 git checkout test-data
 cp -r data ../../cisco/sdwan/test/workflows/playbooks/
